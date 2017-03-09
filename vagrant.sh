fcdcae079dc3ec00e6d1eb3976a468463569b7e5
Nadim Salloum <nadim@salloum.ch>
2017-03-09 21:16:15 +0100

    initial commit

---
+++ vagrant.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOCKFILE=~/scripts/vagrant.lock
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+    echo "already running"
+    exit
+fi
+
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
+cd ~/code/ip_116vt_Lagerverwaltung1/homesteadConfig
+
+if ! vagrant status | grep -q 'poweroff';
+then
+	echo "Vagrant box is currently online"
+	vagrant halt
+else
+	echo "Vagrant box is currently offline"
+	vagrant up
+fi
+
+rm -f ${LOCKFILE}
